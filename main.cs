40cc0a61ea9a55cbec22acf0439bc5d465a43b6f
ueredeveloper <ueredeveloper@gmail.com>
2023-06-05 11:36:53 +0000

    terminando aula syntax

--- main.cs
+++ main.cs
@@ -2,11 +2,17 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World W3Schools C#");
-    // método que soma dois parâmetros
-    void sum (int a, int b){
-      Console.WriteLine(a+b);
-    }
-    sum(1,2);
+
+    Console.WriteLine("Line 1: using System => using class System...");
+
+    Console.WriteLine("Line 2: C# ignore white space...");
+
+    Console.WriteLine("Line 3: namespace organize your code...");
+
+    Console.WriteLine("Line 4: curly braces {...} ...");
+
+    Console.WriteLine("Line 5: class  is a container ...");
+
+    
   }
 }
